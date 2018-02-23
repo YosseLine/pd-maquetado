c1a2106c9b6a2b3ea3a936ce391a87f560bd1764
YosseLine <yoseline_96@hotmail.com>
2018-02-23 06:45:31 -0500

    Subiendo

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+/*
+  function getNews() {
+    const articleRequest = new XMLHttpRequest();
+    articleRequest.open('GET', `https://api.mercadolibre.com/items/:id`);
+    articleRequest.onload = addNews;
+    articleRequest.onerror = handleError;
+    articleRequest.send();
+  }
+
+  function handleError() {
+    console.log('Se ha presentado un error');
+  }
+  function addNews() {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+  }
+  getNews();*/
+
+});
